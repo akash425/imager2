b18bbb69c86b183531fd3564d1728331cbe80d0c
Akash Kushwaha <akashkushwaha425@gmail.com>
2021-02-06 19:44:50 +0530

    running

--- imager/imager/urls.py
+++ imager/imager/urls.py
@@ -1,7 +1,7 @@
-"""imager URL Configuration
+"""image_optimizer URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path("", include("core.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
